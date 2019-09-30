08c5ac24f8c341a36ec40a1e1f37a4475c3a33ca
yejin8861 <yejin8861@naver.com>
2019-09-30 17:58:39 +0900

    add gitignore

--- 2019kakao/d.py
+++
@@ -1,7 +0,0 @@
-a = [(0,),(1,),(0,1),(0,1,2)]
-b = (0,1)
-#c3 = [ list(set(b).intersection(i)) for i in a]
-#c = [list(i) for i in a if list(b) in list(i)]
-c = [i for i in a if set(b).intersection(set(i))]
-print(set(a) - set(c))
-print(c)
