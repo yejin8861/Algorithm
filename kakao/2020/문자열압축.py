28b17de0b2bcbee82510d5a7cb731922ede98ef9
Yejin Kim <yejin8861@naver.com>
2020-05-03 23:25:05 +0900

    2020 kakao 문자열압축

---
+++ kakao/2020/문자열압축.py
@@ -0,0 +1,23 @@
+def solution(s):
+    answer = len(s)
+    for l in range(1, len(s) // 2 + 1):
+        sum = 0
+        cnt = 1
+        for i in range(0, len(s), l):
+            if s[i:i+l] == s[i+l:i+2*l]:
+                cnt += 1
+            else:
+                if cnt == 1:
+                    sum += len(s[i:i+l])
+                else:
+                    sum += len(str(cnt)) + len(s[i:i+l])
+                cnt = 1
+        answer = min(answer, sum)
+    print(answer)
+    return answer
+
+
+if __name__ == '__main__':
+    solution("aabbaccc")
+    # solution("ababcdcdababcdcd")
+    # solution("abcabcdede")
